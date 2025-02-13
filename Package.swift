518686f61e893426a175a304139530ff1ef0500e
Salwa Gaida <gaida.salwa@gmail.com>
2025-02-13 02:51:35 +0100

    Ajout module AppDI

--- Package.swift
+++ Package.swift
@@ -5,17 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "AppDI",
+    platforms: [
+        .iOS(.v15) // ✅ Spécifie que le module supporte iOS 13 minimum
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AppDI",
             targets: ["AppDI"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/hmlongco/Factory.git", from: "2.3.0"),
+        .package(path: "../CoreModule")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AppDI"),
+            name: "AppDI",
+            dependencies: ["Factory", "CoreModule"]),
         .testTarget(
             name: "AppDITests",
             dependencies: ["AppDI"]
