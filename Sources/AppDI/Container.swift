35c2bd7136ae85593d46a7969941b527d8d51695
Salwa Gaida <gaida.salwa@gmail.com>
2025-02-17 22:47:51 +0100

    Comment code

--- Sources/AppDI/Container.swift
+++ Sources/AppDI/Container.swift
@@ -5,9 +5,13 @@ import Foundation
 import Factory
 import CoreModule
 
+// On étend `Container` pour ajouter une propriété les propriétés
 public extension Container {
     
+    /// Définition d'une Factory pour créer une instance de `HTTPClientProtocol`
     var httpClient: Factory<HTTPClientProtocol> {
+        // la factory est rattachée au `Container` actuel
+        // définir comment l'instance de `HTTPClientProtocol` est créée
         Factory(self) { HTTPClient() }
     }
 }
